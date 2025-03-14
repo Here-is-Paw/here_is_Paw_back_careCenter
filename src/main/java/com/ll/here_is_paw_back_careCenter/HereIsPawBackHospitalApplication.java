e247d68366febef66347f8ee2cbd98cc73d67c3c
kknaks <benesia93@naver.com>
2025-03-14 20:52:36 +0900

    done

--- src/main/java/com/ll/here_is_paw_back_careCenter/HereIsPawBackHospitalApplication.java
+++ src/main/java/com/ll/here_is_paw_back_careCenter/HereIsPawBackHospitalApplication.java
@@ -6,8 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HereIsPawBackHospitalApplication {
 
+//  @Autowired
+//  private CareCenterService careCenterService; // 서비스 주입
+
   public static void main(String[] args) {
     SpringApplication.run(HereIsPawBackHospitalApplication.class, args);
   }
 
+//  @Bean
+//  public CommandLineRunner runner() {
+//    return args -> {
+//      careCenterService.fetchAllCareCenters(100);
+//    };
+//  }
 }
